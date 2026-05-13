60a4aa9d5c5d3cc2d75cde1437fa79ada04f6c98
Tim Arnold <websynqdesign@gmail.com>
2026-05-12 20:00:52 -0700

    test: add jsdom + testing-library, wire jest-dom matchers

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: false,
+    setupFiles: ['./vitest.setup.ts'],
     include: ['**/*.test.{ts,tsx}'],
     exclude: ['node_modules/**', '.next/**'],
   },
